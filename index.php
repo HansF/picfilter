353d6ae15b534fde88279337c65a4c38936e39b5
Hans Fraiponts <fraiponts@gmail.com>
2013-04-20 19:02:02 +0200

    Bootstrap hello world

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-
 <?php
-// On Windows:
-if (disk_total_space("d:")>1) echo "we hace d";
-if (disk_total_space("f:")>1) echo "we hace f";
-if (disk_total_space("g:")>1) echo "we hace g";
-if (disk_total_space("C:")>1) echo "we hace c";
 
- phpinfo();
+include('inc/header.php');
+// On Windows:
+echo "hello";
+
+// phpinfo();
+include('inc/footer.php');
 
 ?>
