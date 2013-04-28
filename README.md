96b1784d248ef0cf461517ca04753153e8ca79f2
Hans Fraiponts <fraiponts@gmail.com>
2013-04-29 01:12:36 +0200

    Sqlite3 issues ...

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ A picture filter.
 
 SETUP
 =====
+
+* To get an easy setup download http://zwamp.sourceforge.net/ 
+* Download page : http://sourceforge.net/projects/zwamp/files/ (you don't need the 'full' version)
 * C:\xampp\php\php.ini max_execution_time = 3000
--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 
 include('inc/header.php');
 // On Windows:
-echo "hello";
+echo "hello world";
+$db = new SQLite3('mysqlitedb.db');
 
-// phpinfo();
+var_dump($db->querySingle('SELECT username FROM user WHERE userid=1'));
+print_r($db->querySingle('SELECT username, email FROM user WHERE userid=1', true));
+
+
 include('inc/footer.php');
 
 ?>
