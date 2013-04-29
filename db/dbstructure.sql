e4fbcd2398c60e7242d9a297c8a1bef24126e89c
Hans Fraiponts <fraiponts@gmail.com>
2013-04-29 02:19:21 +0200

    db stiff

--- db/dbstructure.sql
+++ db/dbstructure.sql
@@ -1,3 +1,3 @@
 CREATE TABLE couples ( id varchar(250), couple varchar(250));
-CREATE TABLE images ( imagepath varchar(250),couple varchar(250));
+CREATE TABLE images ( path varchar(250),couple varchar(250));
 CREATE INDEX 'pIndex' ON "images" ("path" ASC)
