6199392f8a3af20b6d76551613a076450b558cca
Hans Fraiponts <fraiponts@gmail.com>
2013-04-29 02:23:19 +0200

    Sqlite3 up 'n running.

--- inc/settings.php
+++ inc/settings.php
@@ -1,32 +1,7 @@
 <?php
 
-function sqlite_open($location,$mode) 
-{ 
-    $handle = new SQLite3($location); 
-    return $handle; 
-} 
-function sqlite_query($dbhandle,$query) 
-{ 
-    $array['dbhandle'] = $dbhandle; 
-    $array['query'] = $query; 
-    $result = $dbhandle->query($query); 
-    return $result; 
-} 
-function sqlite_fetch_array(&$result,$type) 
-{ 
-    #Get Columns 
-    $i = 0; 
-    while ($result->columnName($i)) 
-    { 
-        $columns[ ] = $result->columnName($i); 
-        $i++; 
-    } 
-    
-    $resx = $result->fetchArray(SQLITE3_ASSOC); 
-    return $resx; 
-} 
 $password = "admin"; 
-$rootdir = "C:/xampp/htdocs/picfilter/"; //use front slashes in windows also , always put a trailing slash :-)
+$rootdir = "E:/vdrive/web/"; //use front slashes in windows also , always put a trailing slash :-)
 $importpath = $rootdir."images/import/"; //use front slashes in windows also , always put a trailing slash :-)
 $importwatermarkpath = $rootdir."images/watermark/"; //use front slashes in windows also , always put a trailing slash :-)
 $dbpath = $rootdir."db/mysqlitedb.sqlite"; //use front slashes in windows also , always put a trailing slash :-)
