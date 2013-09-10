663ce7699d10dbfbd4822388b3ff27a93d2717ec
Hans Fraiponts <fraiponts@gmail.com>
2013-09-10 03:12:23 +0200

    Cosmetic

--- backchecker.php
+++ backchecker.php
@@ -14,33 +14,26 @@ $db = new SQLite3($dbpath);
   <input type="hidden" name="y1" value="" />
   <input type="hidden" name="x2" value="" />
   <input type="hidden" name="y2" value="" />
-  <input type="submit" name="submit" value="Submit" />
-  <input type="submit" name="niemand" value="Niemand" />
-</form>
+  <input type="submit" name="submit" value="All Fine!" />
+  <input type="submit" name="Blur" value="Blur!" /><br/>
 
-<form action="backchecker.php" method="post">
+
+
   <input type="hidden" name="x1" value="" />
   <input type="hidden" name="y1" value="" />
   <input type="hidden" name="x2" value="" />
   <input type="hidden" name="y2" value="" />
 </form>
+
 
 <?php 
     $result = $db->querySingle('select path from images where background=0 limit 0,1');
     echo "<img ID='editpicture' src='./images/medium/$result' />";
     if (isset($_POST['Blur'])&&($_POST['Blur'] == "Blur!")){
           print_r($_POST); 
-		  Makeblur(  $importpath, $result, 10, 20, 20, 20);
+		 // Makeblur(  $importpath, $result, 10, 20, 20, 20);
     }
    ?>
-
-<form action="backchecker.php" method="post">
-  <input type="hidden" name="x1" value="" />
-  <input type="hidden" name="y1" value="" />
-  <input type="hidden" name="x2" value="" />
-  <input type="hidden" name="y2" value="" />
-  <input type="submit" name="Blur" value="Blur!" />
-</form>
 <?php 
 
 include('inc/footer.php');
